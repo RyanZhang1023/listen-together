396e2cbcd3d657f0dc813505ef7c6aa60e766776
Ryan Zhang <zwd071023@outlook.com>
2026-03-03 02:15:38 +0800

    qqmusic

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 import {
   searchWithKeyword,
@@ -8,6 +9,7 @@ import {
 } from "./qq-music-api.js";
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server);
 
